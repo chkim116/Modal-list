59c05d58f74e937174a977f36e95cab8d7aab3f2
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-25 00:45:17 +0900

    개별삭제 해야한다

--- javascript.js
+++ javascript.js
@@ -2,58 +2,69 @@ const wrapList = document.querySelector(".wrap__list");
 const wrapModal = document.querySelector(".wrap");
 const submitAddList = document.querySelector(".modal__add");
 const listInput = submitAddList.querySelector("input");
-
 const closeModal = document.querySelector(".modal__closed");
 const clickAddList = document.querySelector(".modal__addbtn");
 const listAllDelBtn = document.querySelector(".navbar__menu-remove");
-
+const section = document.querySelector("#section");
 const SHOPLIST = "shopp__list";
 
 let thisList = [];
 
 // 리스트 전부 삭제
 
-function listAllremove() {
-  const div = wrapList.querySelector("div");
-  localStorage.removeItem("list");
-  thisList = [];
-  wrapList.style.display = "none";
+function listAllRemove() {
   while (wrapList.firstChild) {
     wrapList.removeChild(wrapList.lastChild);
   }
+  localStorage.removeItem("list");
+  thisList = [];
+  wrapList.style.display = "none";
 }
-listAllDelBtn.addEventListener("click", listAllremove);
+listAllDelBtn.addEventListener("click", listAllRemove);
+
+// 하나만 삭제
+
+function listRemove(e) {
+  const delNode = e.target.parentNode;
+  wrapList.removeChild(delNode);
+  const cleanToDos = thisList.filter(function (list) {
+    return list.id !== parseInt(delNode.id);
+  });
+  console.log(cleanToDos);
+  thisList = cleanToDos;
+  saveList();
+}
 
 // 로컬스토리지 저장
-
-function saveList(value) {
+function saveList() {
   localStorage.setItem("list", JSON.stringify(thisList));
 }
-
 // html요소 생성/ push를 이용한 배열 저장 / 객체화를 통한 id 생성(개별로 지울 때)
 
 function createList(value) {
   const wrapLists = document.querySelector(".wrap__list");
   const div = document.createElement("div");
   const span = document.createElement("span");
-  span.innerText = "X";
   div.classList.add(SHOPLIST);
-  div.appendChild(span);
-  div.innerText = value;
   wrapLists.appendChild(div);
+  span.innerText = "X";
+  span.addEventListener("click", listRemove);
+  div.innerText = value;
+  div.appendChild(span);
+  // 문자열에는 innerText 후 child를 추가하는 거다.
   let Id = thisList.length + 1;
   listObj = {
     text: value,
     id: Id,
   };
   thisList.push(listObj);
-  saveList(value);
+  saveList();
   wrapList.style.display = "block";
 }
 
 function init() {
   const loadList = localStorage.getItem("list");
-  if (loadList === null) {
+  if (loadList === null || loadList === "{}") {
     wrapList.style.display = "none";
   } else {
     wrapList.style.display = "block";
@@ -80,7 +91,7 @@ function submitAdd(event) {
     listInput.value = "";
     closedModal();
   } else {
-    alert("입력해주세요");
+    alert("입력해 주세요");
   }
 }
 
